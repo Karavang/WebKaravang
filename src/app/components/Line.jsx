19f58e8f0337915112427828043ad64569e8dd09
Karavang <k4r4v4ng@gmail.com>
2023-12-02 02:25:48 +0100

    adaptive line

--- src/app/components/Line.jsx
+++ src/app/components/Line.jsx
@@ -1,14 +1,35 @@
-import React from "react";
-import line from "../img/SVG/line.svg";
-import Image from "next/image";
+"use client";
+
 const Line = () => {
+  const width = window.innerWidth < 576 ? "10px" : "1.5px";
+  const color = window.innerWidth < 576 ? "#6730b9" : "#422163";
+
   return (
     <div className="line-container">
-      <Image
-        className="line"
-        src={line}
-        alt="Line"
-      />
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 1920.01 27.5"
+      >
+        <defs>
+          <style>
+            {`.cls-1{fill:none;stroke:${color};stroke-miterlimit:10;stroke-width:${width}}`}
+          </style>
+        </defs>
+        <g
+          id="\u0421\u043B\u043E\u0439_2"
+          data-name="\u0421\u043B\u043E\u0439 2"
+        >
+          <g
+            id="\u0421\u043B\u043E\u0439_1-2"
+            data-name="\u0421\u043B\u043E\u0439 1"
+          >
+            <path
+              d="M0 26.41h1270M1313.61 1l-43.35 25.85M1920.01.84 1313.86.75"
+              className="cls-1"
+            />
+          </g>
+        </g>
+      </svg>
     </div>
   );
 };
