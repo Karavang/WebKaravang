a4167196e1dd5b1889fb6b766c2ef0b8381ac0dd
Karavang <k4r4v4ng@gmail.com>
2024-01-15 16:53:55 +0100

    robots

--- src/app/page.js
+++ src/app/page.js
@@ -2,9 +2,8 @@ import { Header } from "./components/Header";
 import { Hero } from "./components/Hero";
 import Line from "./components/Line";
 import { MyHistoryInWeb } from "./components/MyHistoryInWeb";
-import { Shetagri } from "./components/Shetagri";
-import { Work } from "./components/Work";
 import { MyWorks } from "./components/MyWorks";
+import { Work } from "./components/Work";
 
 export default function Home() {
   return (
